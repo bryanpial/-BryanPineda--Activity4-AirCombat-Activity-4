9e5c7f96ec38a0beca676c051271d653a61873f8
Bryan Styven Pineda Albarracin <brayanpial@gmail.com>
2018-10-06 23:51:35 -0500

    no message

---
+++ actividad4/Assets/scrpits/f15.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class f15 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        if(Input.GetKey(KeyCode.RightArrow)){
+            this.transform.Translate(Vector3.right);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            this.transform.Translate(Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow)){
+            this.transform.Translate(Vector3.up);
+        }
+         
+        if (Input.GetKey(KeyCode.DownArrow)){
+            this.transform.Translate(Vector3.down);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        
+          
+        
+       
+      
+
+    
+}
---
+++ actividad4/Assets/scrpits/missile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class missile : MonoBehaviour {
+
+    public float speed;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+
+        this.transform.Translate(Vector3.forward * speed);
+
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Colision detectada");
+
+                
+       
+    }
+}
+
